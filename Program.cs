3ba2ce84249de5b3bc1ba5c23a72509cfc6def4e
Helen <helen@Helens-MacBook-Pro.local>
2018-10-11 09:31:45 -0500

    Aggregate Operations

--- Program.cs
+++ Program.cs
@@ -74,9 +74,36 @@ namespace linqexercise
 
             foreach (int A in collection)
         {
-            Console.WriteLine($"{A}");
+            // Console.WriteLine($"{A}");
         }
 
+        // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+        {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+        };
+            // IEnumerable<int> collectNums = from B in numbers3
+            //     select B;
+            Console.WriteLine($"The number of integers is {numbers3.Count()}");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            IEnumerable<double> collectNums2 = from C in purchases
+                select C;
+            Console.WriteLine($"The total is {collectNums2.Sum()}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+                {
+                    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+                };
+                // IEnumerable<double> highestPrice = from D in prices
+                // orderby D descending
+                // select D;
+                Console.WriteLine($"The highest price is  {prices.Max()}");
         }
     }
 }
