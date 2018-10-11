1b0ad6cf4d2374a0b5a052e1045864a30d69c1ba
Helen <helen@Helens-MacBook-Pro.local>
2018-10-11 14:04:47 -0500

    custom types

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@ using System.Collections.Generic;
 
 namespace linqexercise
 {
+
+    public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
     class Program
     {
         static void Main(string[] args)
@@ -132,16 +139,7 @@ namespace linqexercise
 
         // Using Custom Types
         // Build a collection of customers who are millionaires
-public class Customer
-{
-    public string Name { get; set; }
-    public double Balance { get; set; }
-    public string Bank { get; set; }
-}
-
-public class Program
-{
-    public static void Main() {
+    
         List<Customer> customers = new List<Customer>() {
             new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
             new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
@@ -154,9 +152,52 @@ public class Program
             new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
             new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
         };
-    }
+
+        var millionaires = customers.Where(n => n.Balance >= 1_000_000);
+        foreach (Customer AB in millionaires)
+        {
+            Console.WriteLine($"{AB.Name} has a Balance of {AB.Balance}");
+        }
+ Console.WriteLine("------------------------");
+
+        var millionaires2 = from mill in customers
+        where mill.Balance >= 1_000_000
+        group mill by mill.Bank into BankList
+        select new {
+            ReportBank = BankList.Key,
+            MillPerBank = BankList.ToList().Count()
+        };
+
+        foreach(var bank in millionaires2 )
+        {
+            Console.WriteLine($"{bank.ReportBank}: {bank.MillPerBank}");
+        }
+
+            //  //dealing with kids list
+            //     group customer by customer.Bank into BankGroup
+            // //now dealing with neighborhoodGroup list
+            //     select new SalesReportEntry {
+            //         ReportNeighborhood = neighborhoodGroup.Key, 
+            //         ReportTotalSales = neighborhoodGroup.Sum(kidObj => kidObj.Sales)
+            //     }).OrderByDescending(sr => sr.ReportTotalSales).ToList();
+
+
+        /*
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+        */
+
+
+
+
 }
 
             }
     }
-}
+
